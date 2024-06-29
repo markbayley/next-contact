181d1d9841ea31cdf8925495bd38ded7946df82e
Mark <markyb152@hotmail.com>
2024-06-29 21:51:01 +1000

    fixes

--- components/login-btn.js
+++ components/login-btn.js
@@ -4,15 +4,15 @@ export default function Component() {
   const { data: session } = useSession();
   if (session) {
     return (
-      <>
+      <div>
         Signed in as {session.user.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
-      </>
+      </div>
     );
   }
   return (
     <>
-      {/* Not signed in <br /> */}
+      Not signed in <br />
       <button
         onClick={() => signIn()}
         className="bg-blue-600 rounded py-2 px-4 hover:bg-blue-500 active:scale-95 transition duration-150 font-semibold "
